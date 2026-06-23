7ab8642e438aee4d5ce6a0bf37e98f37e850719a
Teo <168961146+majo-triona@users.noreply.github.com>
2026-06-23 21:30:13 +0300

    Add question selection and 50/50 lifeline

--- game.c
+++ game.c
@@ -3,15 +3,87 @@
 #include <time.h>
 
 #include "game.h"
-#include "question.h"
 
-void startGame(){
+static Question* currentQuestion = NULL;
+static int randomSeeded = 0;
+
+static void seedRandomGenerator(void) {
+	if (!randomSeeded) {
+		srand((unsigned)time(NULL));
+		randomSeeded = 1;
+	}
 }
 
-Question* getRandomQuestion(int difficulty){
+static const char* answerLabel(int index) {
+	switch (index) {
+		case 0: return "A";
+		case 1: return "B";
+		case 2: return "C";
+		case 3: return "D";
+		default: return "?";
+	}
 }
 
-void joker5050(Question* q){
+void startGame() {
+	seedRandomGenerator();
+	currentQuestion = NULL;
+}
+
+Question* getRandomQuestion(int difficulty) {
+	seedRandomGenerator();
+
+	if (questionCount <= 0) {
+		currentQuestion = NULL;
+		return NULL;
+	}
+
+	int matchingIndexes[MAX_QUESTIONS];
+	int matchingCount = 0;
+
+	for (int i = 0; i < questionCount; i++) {
+		if (questions[i].difficulty == difficulty) {
+			matchingIndexes[matchingCount++] = i;
+		}
+	}
+
+	if (matchingCount == 0) {
+		currentQuestion = NULL;
+		return NULL;
+	}
+
+	int selectedIndex = matchingIndexes[rand() % matchingCount];
+	currentQuestion = &questions[selectedIndex];
+	return currentQuestion;
+}
+
+void joker5050(Question* q) {
+	Question* question = NULL;
+
+	if (q != NULL) {
+		question = q;
+	} else {
+		question = currentQuestion;
+	}
+
+	if (question == NULL) {
+		printf("Няма активен въпрос!\n");
+		return;
+	}
+
+	int wrongAnswers[3];
+	int wrongCount = 0;
+
+	for (int i = 0; i < 4; i++) {
+		if (i != question->correctAnswer) {
+			wrongAnswers[wrongCount++] = i;
+		}
+	}
+
+	int keptWrongAnswer = wrongAnswers[rand() % wrongCount];
+
+	printf("50/50 жокер - остават:\n");
+	printf("%s: %s\n", answerLabel(question->correctAnswer), question->answers[question->correctAnswer]);
+	printf("%s: %s\n", answerLabel(keptWrongAnswer), question->answers[keptWrongAnswer]);
 }
 
 int friendHelp(Question* q){
